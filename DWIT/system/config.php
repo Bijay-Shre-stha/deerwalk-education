395e49f42b83217b824a178baa1add359629c76e
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-09-18 20:14:34 +0545

    fixed issue

--- DWIT/system/config.php
+++ DWIT/system/config.php
@@ -4,7 +4,6 @@ define('DB_SERVER','localhost');
 define('DB_USER', 'dwit');
 // define('DB_USER', 'root');
 // define('DB_PASSWORD', '');
-
 define('DB_PASSWORD', 'dAkbG5');
 define('DB_NAME','newdwit');
 define('EMAIL','admission@deerwalk.edu.np');
